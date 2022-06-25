dc13b137c43114fb1bc1312a7a2a9bc7b7692e2f
21989 <21989@tencent.com>
2022-06-25 13:56:01 +0800

    zj4

---
+++ dubbo-provider1/src/test/java/com/mzl/dubboprovider1/DubboProvider1ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.mzl.dubboprovider1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboProvider1ApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
